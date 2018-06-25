f44aa6f136e4398eded6fb038295a533a3bbea3c
gabrielo <espinola.gabriel@gmail.com>
2018-06-25 20:56:11 +0000

    creating project

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: 'java', pages: 101},
+        {title: 'harry potter', pages: 234},
+        {title: 'lord of the rings', pages: 434},
+        {title: 'game of thrones', pages: 13}
+    ];
+}
